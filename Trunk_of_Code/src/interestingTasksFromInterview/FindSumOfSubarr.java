182a0db073753b36d005dcf605cf70883b9ddfc9
Iren <nor.iren@yandex.ru>
2017-07-23 15:57:52 +0300

    add

---
+++ Trunk_of_Code/src/interestingTasksFromInterview/FindSumOfSubarr.java
@@ -0,0 +1,29 @@
+package interestingTasksFromInterview;
+
+import java.util.Scanner;
+
+public class FindSumOfSubarr {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int locSum = 0;
+		
+		int []arr = new int [n];
+		
+		for(int i=0;i<n;i++){
+			arr[i] = in.nextInt();
+		}
+		
+		for(int i = 0; i<n-m;i++){
+			for (int j=0;j<m;j++){
+				locSum+=arr[i+j];
+			}
+			System.out.print(locSum + " ");
+			locSum = 0;
+		}
+
+		in.close();
+	}
+}
--- Trunk_of_Code/src/interestingTasksFromInterview/WriteSingle.java
+++ Trunk_of_Code/src/interestingTasksFromInterview/WriteSingle.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
  *
  */
 
-
 public class WriteSingle {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
